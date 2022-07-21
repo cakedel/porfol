444a75ed050cdd89ede24b9be144df21c72989d2
peonyblue <coalgreyy9@gmail.com>
2022-07-21 17:50:02 +0900

    22.07.21 art coding

--- carandache/js/main.js
+++ carandache/js/main.js
@@ -1,14 +1,13 @@
 $(function () {
-$('.gnb .left').hover(function(e){
-    e.preventDefault()
-    $('.smenu').stop().slideUp()
-    $('.smenu').stop().slideToggle()
-    $('.gnb').addClass('hover')
-})
-$('.gnb .left').mouseleave(function(){
-    $('.gnb').removeClass('hover')
-})
-
+    $('.gnb .left').hover(function (e) {
+        e.preventDefault()
+        $('.gnb').addClass('hover')
+        $('.smenu').stop().slideToggle()
+    },
+        () => {
+            $('.gnb').removeClass('hover')
+            $('.smenu').stop().slideUp()
+        })
     $('.mainVisual').YTPlayer({
         videoURL: 'https://youtu.be/SdAdcDa2xRA',
         containment: '.video', autoPlay: true, mute: true, startAt: 5, opacity: 1, showControls: false, playOnlyIfVisible: true,
@@ -18,6 +17,7 @@ $('.gnb .left').mouseleave(function(){
         autoplay: true,
         centerMode: true,
         pauseOnHover: false,
+        pauseOnFocus: false,
         centerPadding: '700px',
     });
     $(window).scroll(function () {
@@ -25,27 +25,50 @@ $('.gnb .left').mouseleave(function(){
         var sct = $(document).scrollTop()
 
         if (sct > $('.mainSlider').offset().top) {
-            $('header .gnb').addClass('on')
+            $('header .gnb').addClass('on');     
         }
         else {
-            $('header .gnb').removeClass('on')
+            $('header .gnb').removeClass('on');       
+        }
+        if (sct > $('.mainSlider').offset().top / 2) {
+            $('.mainSlider span').addClass('on');
+            $('.mainSlider h2').addClass('on');
+            $('.mainSlider p').addClass('on');
+            $('.mainSlider h2').addClass('on');
+            $('.mainSlider figure').addClass('on');
+            $('.slider').slick('slickPlay');  
+        }
+        else {
+            $('.mainSlider span').removeClass('on');
+            $('.mainSlider h2').removeClass('on');
+            $('.mainSlider p').removeClass('on');
+            $('.mainSlider figure').removeClass('on');
+            $('.slider').slick('slickPause');  
         }
 
-        if (sct < $('.mainSlider').offset().top) {
-            $('.slider').slick('slickPause');
-        }
-        else {
-            $('.slider').slick('slickPlay');
-        }
     });
+
+    $('.tMenu li').on('click', function (e) {
+        e.preventDefault();
+        $('.tMenu li').removeClass('on')
+        $(this).addClass('on');
+
+        var idx = $(this).index()
+        $('.pSlider').removeClass('on');
+        $('.pSlider').eq(idx).addClass('on');
+        $('.pSlider').slick('setPosition');
+    });
+
     $('.pSlider').slick({
         slidesToShow: 3,
         arrows: false,
-        autoplay: true,
 
     });
-    $('.tMenu>li').on('click', () => {
-        console.log('on')
-        $(this).addClass('on')
+
+    $('.pArrows .left').on('click', function () {
+        $('.pSlider').slick('slickPrev');
+    })
+    $('.pArrows .right').on('click', function () {
+        $('.pSlider').slick('slickNext');
     });
 });
