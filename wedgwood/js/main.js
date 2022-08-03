db2396b50c8cf2b3c4a8be533ddd70ac37ac7708
peonyblue <coalgreyy9@gmail.com>
2022-08-03 17:59:00 +0900

    22.08.03 wedg coding

--- wedgwood/js/main.js
+++ wedgwood/js/main.js
@@ -5,7 +5,7 @@ $(function () {
     })
     $('.sumSlider').slick({
         slidesToShow: 4,
-        slidesToScroll :2,
+        slidesToScroll: 2,
         arrows: false,
     });
     $('.popArrows i:nth-child(1)').on('click', function () {
@@ -14,4 +14,14 @@ $(function () {
     $('.popArrows i:nth-child(2)').on('click', function () {
         $('.sumSlider').slick('slickNext')
     });
+    $('.tabMenu').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed:5000,
+        cssEase: 'linear',
+        slidesToShow: 5,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    })
 });
