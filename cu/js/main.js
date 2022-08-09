199fe85a34bff7b0328bffe5b37aee5792a18a4e
peonyblue <coalgreyy9@gmail.com>
2022-08-09 17:50:36 +0900

    22.08.09 gs coding

--- cu/js/main.js
+++ cu/js/main.js
@@ -1,7 +1,14 @@
 $(function () {
     $('.mainSlider').slick({
         arrows: false,
-        centerMode:true,
-        centerPadding:'500px',
+        centerMode: true,
+        centerPadding: '500px',
+        dots: true,
+    })
+    $('.arrows i:nth-child(1)').on('click', function () {
+        $('.mainSlider').slick('slickPrev')
+    })
+    $('.arrows i:nth-child(2)').on('click', function () {
+        $('.mainSlider').slick('slickNext')
     })
 });
