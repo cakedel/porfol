945f00368ae07b756ea17ce31197dac81a3d09c3
cakedel <coalgreyy9@gmail.com>
2022-08-18 16:21:33 +0900

    cu coding

--- cu/js/main.js
+++ cu/js/main.js
@@ -4,11 +4,41 @@ $(function () {
         centerMode: true,
         centerPadding: '500px',
         dots: true,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    centerMode: false,
+                    centerPadding: '0',
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     })
     $('.arrows i:nth-child(1)').on('click', function () {
         $('.mainSlider').slick('slickPrev')
     })
     $('.arrows i:nth-child(2)').on('click', function () {
         $('.mainSlider').slick('slickNext')
+    })
+    if ($(window).width() < 1000) {
+        $('.proGrid').slick({
+            arrows: false,
+        })
+    }
+        $(window).resize(function () {
+            if ($(window).width() > 1000) {
+                $('.proGrid').slick('unslick')
+            }
+            else {
+                $('.proGrid').slick({
+                    arrows: false,
+                })
+            }
+        })
+    $('.mbtn').on('click', function () {
+        $(this).toggleClass('on')
+        $('.gnb').toggleClass('on')
     })
 });
