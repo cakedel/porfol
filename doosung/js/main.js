1b149052760dcac8bf50aca3b235318e985463ac
cakedel <coalgreyy9@gmail.com>
2022-08-25 17:45:31 +0900

    doosung coding

--- doosung/js/main.js
+++ doosung/js/main.js
@@ -1,20 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-        const productSwiper = new Swiper('.productSlider', {
-            slidesPerView: 4,
-            spaceBetween : 30,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            scrollbar: {
-                el: '.swiper-scrollbar',
-              },
-        });
-        const floorExplain = new Swiper('.floorExplain', {
-            slidesPerView: 1,
-        });
-        const floorSlider = new Swiper('.floorSlider', {
-            slidesPerView: 1,
-            spaceBetween : 30,
-        });
+    const productSwiper = new Swiper('.productSlider', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.productNext',
+            prevEl: '.productPrev',
+        },
+        scrollbar: {
+            el: '.productScroll',
+        },
+    });
+    const floorExplain = new Swiper('.floorExplain', {
+        slidesPerView: 1,
+        loop : true,
+        effect: 'fade',
+        navigation: {
+            nextEl: '.floorNext',
+            prevEl: '.floorPrev',
+        },
+    });
+    const floorSlider = new Swiper('.floorSlider', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop : true,
+        navigation: {
+            nextEl: '.floorNext',
+            prevEl: '.floorPrev',
+        },
+    });
+    const floorImage = new Swiper('.floorImage', {
+        slidesPerView: 1,
+        loop : true,
+        effect: 'fade',
+        navigation: {
+            nextEl: '.floorNext',
+            prevEl: '.floorPrev',
+        },
+    });
+
+
 });
