96172ef8f66fcaf7e03991510f551e70e579efcf
cakedel <coalgreyy9@gmail.com>
2022-09-02 07:40:56 +0900

    clone coding

--- dmuseum/js/main.js
+++ dmuseum/js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+
     const whatSlider = new Swiper('.whatSlider', {
         spaceBetween: 0,
         loop: true,
@@ -6,15 +7,26 @@ document.addEventListener('DOMContentLoaded', function () {
             nextEl: '.next',
             prevEl: '.prev',
         },
+        on: {
+            'init': function () {
+                document.querySelector('.num').innerHTML = (this.realIndex + 1) + " / " + (this.slides.length - 2)
+            }
+        }
+
+    })
+
+    whatSlider.on('slideChangeTransitionEnd', function () {
+
+        document.querySelector('.num').innerHTML = (whatSlider.realIndex + 1) + " / " + (whatSlider.slides.length - 2)
     })
 
     const infoSlider = new Swiper('.infoSlider', {
         loop: true,
     })
+
+
     whatSlider.controller.control = infoSlider;
     infoSlider.controller.control = whatSlider;
-
-    document.querySelector('.num').innerHTML = (whatSlider.realIndex + 1) + " / " + (whatSlider.slides.length - 2)
 
 
     const tabMenu = document.querySelectorAll('.shop .tabMenu li')
@@ -23,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
         el.addEventListener('click', function () {
             tabMenu.forEach(el => el.classList.remove('on'))
             el.classList.add('on')
-            tabContent.forEach(el=> el.classList.remove('on'))
+            tabContent.forEach(el => el.classList.remove('on'))
             tabContent[idx].classList.add('on')
         })
     })
