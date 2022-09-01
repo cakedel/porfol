830baa6e05fc5c65217324b0e59302abf4c0c98f
cakedel <coalgreyy9@gmail.com>
2022-09-01 22:41:09 +0900

    clone coding

--- dmuseum/js/main.js
+++ dmuseum/js/main.js
@@ -15,4 +15,16 @@ document.addEventListener('DOMContentLoaded', function () {
     infoSlider.controller.control = whatSlider;
 
     document.querySelector('.num').innerHTML = (whatSlider.realIndex + 1) + " / " + (whatSlider.slides.length - 2)
+
+
+    const tabMenu = document.querySelectorAll('.shop .tabMenu li')
+    const tabContent = document.querySelectorAll('.shop .right .tabContent')
+    tabMenu.forEach((el, idx) => {
+        el.addEventListener('click', function () {
+            tabMenu.forEach(el => el.classList.remove('on'))
+            el.classList.add('on')
+            tabContent.forEach(el=> el.classList.remove('on'))
+            tabContent[idx].classList.add('on')
+        })
+    })
 })
